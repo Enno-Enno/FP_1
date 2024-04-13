dcd1abf960a2c6f8603d5db125329d391f9cc8a8
Enno Wellmann <enno.wellmann@tu-dortmund.de>
2024-04-13 20:25:41 +0200

    doofes makefile :(

--- directrory.sh
+++ directrory.sh
@@ -6,58 +6,29 @@ read Name
 mkdir -p $dir
 printf "all: build/$Name.pdf
 
-# hier Python-Skripte:
-build/plot.pdf: plot.py ../matplotlibrc ../header-matplotlib.tex | build 
-        # so that matplotlib can find the tex header when running        
-        # LaTeX in the tmp directory
-        # and set the matplotlibrc
-        TEXINPUTS=\$\$(pwd)/..: MATPLOTLIBRC=../matplotlibrc python plot.py
+build/plot1.pdf: plot1.py data1.txt | build
+	python plot1.py
 
-# hier weitere Abhängigkeiten für build/$Name.pdf deklarieren:
-build/$Name.pdf: build/plot.pdf
-
-build/$Name.pdf: FORCE | build
-\t\t# to find header and bib files in the main directory
-\t\tTEXINPUTS=..: \\
-\t\tBIBINPUTS=..: \\
-\t\tmax_print_line=1048576 \\
-\t\tlatexmk \\
-\t\t  --lualatex \\
-\t\t  --output-directory=build \\
-\t\t  --interaction=nonstopmode \\
-\t\t  --halt-on-error \\
-\t\t$Name.tex
+build/$Name.pdf: $Name.tex | build
+	lualatex --output-directory=build --interaction=batchmode --halt-on-error $Name.tex
+	BIBINPUTS=..:
+	biber build/$Name.bcf
+	lualatex --output-directory=build --interaction=batchmode --halt-on-error $Name.tex
 
 build:
-\t\tmkdir -p build
+	mkdir -p build
 
 clean:
-\t\trm -rf build
-
-FORCE:
+	rm -rf build
 
 .PHONY: all clean" > $dir/Makefile
-echo "\\input{header.tex}      
-
-\subject{VERSUCH NUMMER}
-\\title{TITEL}
-\\date{%
-  Durchführung: DATUM
-  \hspace{3em}
-  Abgabe: DATUM
-}
-
-\\begin{document}
-
-\\maketitle
-\\thispagestyle{empty}
-\\tableofcontents
-\\newpage
-
-\\section{Theorie}
-
-\\printbibliography{}
-
-\\end{document}" > $dir/$Name.tex
-echo "import matplotlib.pyplot as plt
-import numpy as np" > $dir/plot.py
+cd $dir
+cp -r ../Vxxx/Bilder .
+cp ../Vxxx/plot1.py .
+touch data1.txt
+cp ../Vxxx/Vxxx.tex .
+cp ../Vxxx/lit.bib
+cp ../Vxxx/programme.bib
+mv Vxxx.tex $Name.tex
+make
+cd ..
