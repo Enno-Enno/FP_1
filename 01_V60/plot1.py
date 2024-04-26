46528fe08947671db700c060f8ecc85e38e09055
Enno Wellmann <eh.wellmann@gmx.de>
2024-04-26 20:20:32 +0200

    Auswertung und Diskussion v60

--- 01_V60/plot1.py
+++ 01_V60/plot1.py
@@ -5,3 +5,6 @@ import uncertainties as unc
 import uncertainties.unumpy as unp
 
 x=np.genfromtxt('data1.txt', unpack=True)
+
+Schwellwert_unten = np.array([34.8,34.6])
+Schwellwert_oben = np.array([36,34.9, 34.8])
