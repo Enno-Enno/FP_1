9b13100981fdb1d1c2dc3db219524f384d673be0
Enno Wellmann <eh.wellmann@gmx.de>
2024-05-05 16:27:38 +0200

    [V18] WQ Compton streuung

--- 02_V18/plot1.py
+++ 02_V18/plot1.py
@@ -1,7 +1,23 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy.optimize import curve_fit 
+from scipy.optimize import curve_fit
 import uncertainties as unc
 import uncertainties.unumpy as unp
 
-x=np.genfromtxt('data1.txt', unpack=True)
+x = np.genfromtxt("data1.txt", unpack=True)
+
+
+def WQ_compton(theta):
+    cos = lambda x: np.cos(x)
+    WQ = (
+        Z
+        * r_0**2
+        * (1 / (alpha * (1 - cos(theta)) + 1)) ** 2
+        * (cos(theta) ** 2 + 1) / 2
+        * (
+            (alpha**2 * (1 - cos(theta)) ** 2)
+            / (((alpha * (-cos(theta) + 1) + 1) * (cos(theta) ** 2 + 1)))
+            + 1
+        )
+    )  # - Derivative(sigma, Omega) == 0
+    return WQ
