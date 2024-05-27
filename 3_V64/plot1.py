cd1cb6f88bd7a2f024ad4fa1e65f2d1bc2d2573d
Enno Wellmann <eh.wellmann@gmx.de>
2024-05-27 20:46:17 +0200

    Daten eingetragen

--- 3_V64/plot1.py
+++ 3_V64/plot1.py
@@ -1,7 +1,5 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy.optimize import curve_fit 
+from scipy.optimize import curve_fit
 import uncertainties as unc
 import uncertainties.unumpy as unp
-
-x=np.genfromtxt('data1.txt', unpack=True)
--- Vxxx/plot1.py
+++ Vxxx/plot1.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy.optimize import curve_fit 
+from scipy.optimize import curve_fit
 import uncertainties as unc
 import uncertainties.unumpy as unp
 
