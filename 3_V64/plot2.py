5b649efd1ee64b97b52a36a01d3982aa135478a5
Enno Wellmann <eh.wellmann@gmx.de>
2024-05-30 21:19:23 +0200

    Zwischencommit

--- 3_V64/plot2.py
+++ 3_V64/plot2.py
@@ -10,8 +10,9 @@ from plot1 import degReturnRad
 pp = pprint.PrettyPrinter(indent=4)
 
 def nGlass(k, rotation_angle_deg):
+    # TODO Richtige Formel Erstellen mit Verkippung des Glases
     lamVac = 632.990 # nm
     theta = degReturnRad(rotation_angle_deg)
-    return 1/(1-k* lamVac* np.cos(theta)/(theta**2))    
-    ...
+    return 1/(1-k* lamVac* np.cos(theta)/(theta**2))#  k = Delta Phi / (2pi)
 
+counter_glass = d.RefIndexGlasNumbers
