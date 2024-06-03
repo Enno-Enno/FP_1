65b75ca335cb361216d85b038b5b45b71145ce7b
Enno Wellmann <eh.wellmann@gmx.de>
2024-06-03 15:23:54 +0200

    alles ist doof

--- 03_V64/plot2.py
+++ 03_V64/plot2.py
@@ -13,21 +13,21 @@ lamVac = 632.990 * 1e-9# m
 counter_glass = d.RefIndexGlasNumbers
 T_0 = 1e-3 #m
 
-## Falsche Formeln
-def T(theta_rad):
-    '''return thickness of glass for the laser'''
-    return T_0/np.cos(theta_rad)
+# Falsche Formeln
+# def T(theta_rad):
+    # '''return thickness of glass for the laser'''
+    # return T_0/np.cos(theta_rad)
     # return T_0
-
-    
-def R(theta_rad):
-    return  2*np.pi/(lamVac) *T(theta_rad) * theta_rad**2
-
-## bis hier
+# 
+    # 
+# def R(theta_rad):
+    # return  2*np.pi/(lamVac) *T(theta_rad) * theta_rad**2
+# 
+# bis hier
 def Rcombined(theta_deg):
     theta_rad = degReturnRad(theta_deg)
     theta_0_deg = 10 
-    theta_02_deg = -10
+    # theta_02_deg = -10
     theta_0_rad = degReturnRad(theta_0_deg) 
     # theta_02_rad = degReturnRad(theta_02_deg) 
     # return (R(theta_01_rad + theta_rad) + R(theta_rad + theta_02_rad))  
@@ -37,58 +37,69 @@ def Rcombined(theta_deg):
 
 
 def nGlass(DeltaPhi):
-    return 1/(1-(2*DeltaPhi)/(Rcombined(10)- Rcombined(0))) 
+    return 1/(1-(DeltaPhi)/(Rcombined(10)))# - Rcombined(0))) 
 
 def getk(n,theta):
     DeltaPhi =  np.abs(Rcombined(theta)- Rcombined(0)) * (n-1)/(2*n)
-    return DeltaPhi /(2*np.pi)
+    return DeltaPhi /(np.pi)
 
 # for val in counter_glass:
     # print(val, r"\\ ")
 
 uCounterGlass = unc.ufloat(np.mean(counter_glass), np.std(counter_glass))
 print(uCounterGlass)
-DeltaPhi = 2* np.pi* uCounterGlass
+DeltaPhi = 2 *np.pi* uCounterGlass
 n = nGlass(DeltaPhi)
 print(n)
 print("Kontrolle", getk(n, 10))
 
 print("Refractive Index air ----------------------------------")
 print(d.counter_air)
-difference_array = np.zeros((np.shape(d.counter_air)[0],np.shape(d.counter_air)[1]-1))
-for i,_ in enumerate(difference_array[0,:]):
-    difference_array[0,i] = d.counter_air[0,i+1] - d.counter_air[0,i]
-    difference_array[1,i] = d.counter_air[1,i+1] - d.counter_air[1,i]
-    difference_array[2,i] = d.counter_air[2,i+1] - d.counter_air[2,i]
-    difference_array[3,i] = d.counter_air[3,i+1] - d.counter_air[3,i]
-
-dcounter = unc.ufloat(np.mean(difference_array),np.std(difference_array))
-print(dcounter)
+# difference_array = np.zeros((np.shape(d.counter_air)[0],np.shape(d.counter_air)[1]-1))
+# for i,_ in enumerate(difference_array[0,:]):
+    # difference_array[0,i] = d.counter_air[0,i+1] - d.counter_air[0,i]
+    # difference_array[1,i] = d.counter_air[1,i+1] - d.counter_air[1,i]
+    # difference_array[2,i] = d.counter_air[2,i+1] - d.counter_air[2,i]
+    # difference_array[3,i] = d.counter_air[3,i+1] - d.counter_air[3,i]
+# 
+# dcounter = unc.ufloat(np.mean(difference_array),np.std(difference_array))
+# print(dcounter)
 endarray = d.counter_air[:,-1]
 end = unc.ufloat(np.mean(endarray),np.std(endarray))
 print(end)
-
+# 
 def deltaN(DeltaPhi_rad, L):
     return DeltaPhi_rad/(2*np.pi*L) * lamVac
 L = unc.ufloat(100, 0.1) *1e-3 #m
 delN = deltaN(end*2*np.pi, L)
 print("Delta n ", delN)
-delNFine = deltaN(dcounter*np.pi, L) * len(difference_array[0,:])
-print(delNFine)
-
+# delNFine = deltaN(dcounter*np.pi, L) * len(difference_array[0,:])
+# print(delNFine)
+# 
 for i,_ in enumerate(d.counter_air[0,:]):
     print(d.pressures_mbar[i], " & ",d.counter_air[0,i], " & ",d.counter_air[1,i], " & ",d.counter_air[2,i], " & ",d.counter_air[3,i], r" \\",)
-
-delN = deltaN(d.counter_air.flatten()*2*np.pi,L)
-nomN = unp.nominal_values(delN)
-
-print(np.shape(nomN))
+# 
+# delN = deltaN(d.counter_air.flatten()*2*np.pi,L)
+# nomN = unp.nominal_values(delN)
+# 
+# print(np.shape(nomN))
 deltaPhis= d.counter_air.flatten()*2*np.pi
-polynomial_fit = np.polyfit(deltaPhis,unp.nominal_values(delN),1)
+pressures_mbar = np.append(d.pressures_mbar,[d.pressures_mbar,d.pressures_mbar,d.pressures_mbar])
+par, cov = np.polyfit(pressures_mbar,unp.nominal_values(deltaN(deltaPhis,L)),1, cov=True)
 
-print(polynomial_fit)
+par = unc.correlated_values(par, cov)
+m = float(unp.nominal_values(par[0]))
+d1 = float(unp.nominal_values(par[1]))
+m_f= float(unp.std_devs(par[0]))
+d1_f= float(unp.std_devs(par[1]))
 
-plt.plot(deltaPhis,nomN- polynomial_fit(deltaPhis),".",label="Dunkelheit")
+print(par)
 
+plt.figure(constrained_layout=True)
+plt.plot(pressures_mbar, unp.nominal_values(deltaN(deltaPhis,L)),".",label="measured values")
+
+plt.xlabel(r"$p/\text{mBar}$")
+plt.ylabel(r" $\Delta n = n-1$")
 plt.legend()
+plt.savefig("build/plot2.pdf")
 plt.show()
