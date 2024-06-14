c181f42cd103bf35d39d1760e71c4e3abbae8bb0
Benedikt Lütke Lanfer <benedikt.luetkelanfer@tu-dortmund.de>
2024-06-14 11:30:43 +0200

    Anfang plot1 V46

--- 04_V46/plot1.py
+++ 04_V46/plot1.py
@@ -4,4 +4,12 @@ from scipy.optimize import curve_fit
 import uncertainties as unc
 import uncertainties.unumpy as unp
 
-x=np.genfromtxt('data1.txt', unpack=True)
+d, B=np.genfromtxt('data1.txt', unpack=True)
+
+th1_n1,th1_n1_m,th2_n1,th2_n1_m, th1_r,th1_r_m,th2_r,th2_r_m, th1_n2,th1_n2_m,th2_n2,th2_n2_m=np.genfromtxt('data1.txt', unpack=True)
+
+
+
+plt.plot(d,B, "rx",label="Messdaten B-Feld")
+plt.legend()
+plt.show()
