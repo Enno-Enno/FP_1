bfc39ec2f412bb15dfe0f9d945189f41fef76662
Benedikt Lütke Lanfer <benedikt.luetkelanfer@tu-dortmund.de>
2024-06-18 15:01:48 +0200

    Plots sind fertig

--- 04_V46/plot1.py
+++ 04_V46/plot1.py
@@ -3,9 +3,10 @@ import numpy as np
 from scipy.optimize import curve_fit
 import uncertainties as unc
 import uncertainties.unumpy as unp
+import scipy.constants as con
 
-def f(x, m, b):
-    return m*x+b 
+def f(x, m):
+    return m*x#+b 
 
 d, B=np.genfromtxt('data1.txt', unpack=True)
 th1_n1,th1_n1_m,th2_n1,th2_n1_m, th1_r,th1_r_m,th2_r,th2_r_m, th1_n2,th1_n2_m,th2_n2,th2_n2_m=np.genfromtxt('data2.txt', unpack=True)
@@ -17,12 +18,13 @@ th2_r=th2_r+th2_r_m/60
 th1_n2=th1_n2+th1_n2_m/60
 th2_n2=th2_n2+th2_n2_m/60
 
-L_n1=1.296
-L_r=5.11
-L_n2=1.36
+L_n1=1.296*10**(-3)
+L_r=5.11*10**(-3)
+L_n2=1.36*10**(-3)
 
-lamda=np.array([1.06,1.29,1.45,1.72,1.96,2.156,2.34,2.51,2.65])
-x=np.linspace(1,8)
+lamda=np.array([1.06,1.29,1.45,1.72,1.96,2.156,2.34,2.51,2.65])#*10**(-6)
+d_x=np.linspace(68,132)
+x=np.linspace(1,8)#*10**(-12)
 
 delta_n1=abs(th1_n1-th2_n1)/(2*L_n1)/180*np.pi
 delta_r=abs(th1_r-th2_r)/(2*L_r)/180*np.pi
@@ -32,62 +34,59 @@ print(delta_n1)
 
 plt.figure(constrained_layout=True)
 plt.plot(d,B, "rx",label="Messdaten B-Feld")
+plt.plot(d_x,0.000000001*x+428,"b--",label="Maximales Feld")
+plt.xlabel(r"Abstand $d \, [\mathrm{mm}]$")
+plt.ylabel(r"$B \, [\mathrm{mT}]$")
+plt.xlim(d_x[0],d_x[-1])
 plt.legend()
-plt.show()
+plt.savefig("build/B_Feld.pdf")
+#plt.show()
 
 plt.figure(constrained_layout=True)
 plt.plot(lamda**2,delta_n1, "rx",label="Messdaten Probe 1")
-plt.ylabel(r"$\theta  [\mathrm{°/mm}]$")
-plt.xlabel(r"$\lambda [\mathrm{\mu m}^2]$")
+plt.plot(lamda**2,delta_r, "bx",label="Messdaten Probe 2")
+plt.plot(lamda**2,delta_n2, "gx",label="Messdaten Probe 3")
+plt.ylabel(r"$\theta  [\mathrm{rad/m}]$")
+plt.xlabel(r"$\lambda^2 [\mathrm{\mu m}^2]$")
 plt.legend()
-plt.show()
-
-plt.figure(constrained_layout=True)
-plt.plot(lamda**2,delta_r, "rx",label="Messdaten Probe 2")
-plt.ylabel(r"$\theta  [\mathrm{°/mm}]$")
-plt.xlabel(r"$\lambda [\mathrm{\mu m}^2]$")
-plt.legend()
-plt.show()
-
-plt.figure(constrained_layout=True)
-plt.plot(lamda**2,delta_n2, "rx",label="Messdaten Probe 3")
-plt.ylabel(r"$\theta  [\mathrm{°/mm}]$")
-plt.xlabel(r"$\lambda [\mathrm{\mu m}^2]$")
-plt.legend()
-plt.show()
+plt.savefig("build/Messdaten.pdf")
+#plt.show()
 
 par, cov=curve_fit(f,lamda**2, abs(delta_n2-delta_r))
 par = unc.correlated_values(par, cov)
 m = float(unp.nominal_values(par[0]))
-b = float(unp.nominal_values(par[1]))
 m_f= float(unp.std_devs(par[0]))
-b_f= float(unp.std_devs(par[1]))
-
 print(m,m_f)
-print(b,b_f)
 
 plt.figure(constrained_layout=True)
-plt.plot(lamda**2,abs(delta_n2-delta_r), "rx",label="Weiß noch nicht")
-plt.plot(x,f(x,m,b),"b-", label="Ausgleichsgerade")
-plt.ylabel(r"$\theta  [\mathrm{°/mm}]$")
-plt.xlabel(r"$\lambda [\mathrm{\mu m}^2]$")
+plt.plot(lamda**2,abs(delta_n2-delta_r), "gx",label=r"$\Delta \theta_1$")
+plt.plot(x,f(x,m),"b-", label="Ausgleichsgerade")
+plt.ylabel(r"$\theta  [\mathrm{rad/m}]$")
+plt.xlabel(r"$\lambda^2 [\mathrm{\mu m}^2]$")
 plt.legend()
-plt.show()
+plt.savefig("build/plot1.pdf")
+#plt.show()
+
+m=unp.uarray(m,m_f)
+m1=unp.sqrt((con.e**3*1.28*10**(24)*0.428)/(8*3.857*con.epsilon_0*m*10**(12)*con.c**3*np.pi**2))
 
 par, cov=curve_fit(f,lamda**2, abs(delta_n1-delta_r))
 par = unc.correlated_values(par, cov)
 m = float(unp.nominal_values(par[0]))
-b = float(unp.nominal_values(par[1]))
 m_f= float(unp.std_devs(par[0]))
-b_f= float(unp.std_devs(par[1]))
-
 print(m,m_f)
-print(b,b_f)
 
 plt.figure(constrained_layout=True)
-plt.plot(lamda**2,abs(delta_n1-delta_r), "rx",label="Weiß noch nicht")
-plt.plot(x,f(x,m,b),"b-", label="Ausgleichsgerade")
-plt.ylabel(r"$\theta  [\mathrm{°/mm}]$")
-plt.xlabel(r"$\lambda [\mathrm{\mu m}^2]$")
+plt.plot(lamda**2,abs(delta_n1-delta_r), "gx",label=r"$\Delta \theta_1$")
+plt.plot(x,f(x,m),"b-", label="Ausgleichsgerade")
+plt.ylabel(r"$\theta  [\mathrm{rad/m}]$")
+plt.xlabel(r"$\lambda^2 [\mathrm{\mu m}^2]$")
 plt.legend()
-plt.show()
+plt.savefig("build/plot2.pdf")
+#plt.show()
+
+m=unp.uarray(m,m_f)
+m2=unp.sqrt((con.e**3*2.8*10**(24)*0.428)/(8*3.857*con.epsilon_0*m*10**(12)*con.c**3*np.pi**2))
+
+print(m1/con.m_e)
+print(m2/con.m_e)
