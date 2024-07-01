3ff3d8fdcadff5f92884436c1e4f169b48ce4606
Enno Wellmann <eh.wellmann@gmx.de>
2024-07-01 20:10:08 +0200

    Chaotischer Praktikumstag

--- 05_V01/plot1.py
+++ 05_V01/plot1.py
@@ -4,4 +4,33 @@ from scipy.optimize import curve_fit
 import uncertainties as unc
 import uncertainties.unumpy as unp
 
-x=np.genfromtxt('data1.txt', unpack=True)
+def f(x, m, d):
+    return m*x+d 
+
+
+
+n_eu=np.genfromtxt('data1.txt', unpack=True)
+
+x=np.zeros(len(n_eu))
+N=np.zeros(len(n_eu))
+
+for index, val in enumerate(n_eu):
+    x[index]=index
+    N[index]=val
+
+plt.plot(x,np.log(N),"kx")
+plt.show()
+
+par, cov=curve_fit(f,x, np.log(N))
+par = unc.correlated_values(par, cov)
+m = float(unp.nominal_values(par[0]))
+d1 = float(unp.nominal_values(par[1]))
+m_f= float(unp.std_devs(par[0]))
+d1_f= float(unp.std_devs(par[1]))
+x_p=np.linspace(0,100)
+
+plt.plot(x,np.log(N),"kx")
+plt.plot(x_p,f(x_p,m,d1),"g-")
+plt.show()
+
+print(m,m_f, d1,d1_f)
