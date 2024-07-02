e4ba181d8a4d72e84ae5480de0f990cb943bc1ac
Benedikt Lütke Lanfer <benedikt.luetkelanfer@tu-dortmund.de>
2024-07-02 11:11:05 +0200

    Auswertung plot

--- 05_V01/plot1.py
+++ 05_V01/plot1.py
@@ -3,34 +3,67 @@ import numpy as np
 from scipy.optimize import curve_fit
 import uncertainties as unc
 import uncertainties.unumpy as unp
+from scipy.signal import find_peaks
 
-def f(x, m, d):
-    return m*x+d 
+def f(x, m, b):
+    return m*x+b 
+
+def exp(t,lamda,N,U):
+    return N*np.exp(-lamda*t) +U
 
 
+N_kal=np.genfromtxt('data2.txt', unpack=True)
+N_myon=np.genfromtxt('data1.txt', unpack=True)
 
-n_eu=np.genfromtxt('data1.txt', unpack=True)
+peak,_=find_peaks(N_kal,distance=10,height=10)
+t_kal=np.arange(1, len(peak)+1)*5-1
+print(peak)
+print(t_kal)
 
-x=np.zeros(len(n_eu))
-N=np.zeros(len(n_eu))
-
-for index, val in enumerate(n_eu):
-    x[index]=index
-    N[index]=val
-
-plt.plot(x,np.log(N),"kx")
-plt.show()
-
-par, cov=curve_fit(f,x, np.log(N))
+par, cov=curve_fit(f,peak, t_kal)
 par = unc.correlated_values(par, cov)
 m = float(unp.nominal_values(par[0]))
-d1 = float(unp.nominal_values(par[1]))
+b = float(unp.nominal_values(par[1]))
 m_f= float(unp.std_devs(par[0]))
-d1_f= float(unp.std_devs(par[1]))
-x_p=np.linspace(0,100)
+b_f= float(unp.std_devs(par[1]))
 
-plt.plot(x,np.log(N),"kx")
-plt.plot(x_p,f(x_p,m,d1),"g-")
+print(m,b)
+x=np.linspace(0,450)
+
+plt.plot(x,f(x,m,b),"m-",label="Ausgleichsgerade")
+plt.plot(peak,t_kal,"bx",label="Messwerte Kalibrierung")
+plt.xlabel(r"Channel")
+plt.ylabel(r"$t [\mathrm{\mu s}]$")
+plt.legend()
 plt.show()
 
-print(m,m_f, d1,d1_f)
+t_myon=np.arange(1, len(N_myon)+1)
+t_myon=f(t_myon,m,b)
+
+plt.plot(t_myon,np.log(N_myon),"bx",label="Messwerte")
+plt.xlabel(r"$t [\mathrm{\mu s}]$")
+plt.legend()
+plt.show()
+
+A=17;B=-1
+
+par, cov=curve_fit(exp,t_myon[A:B],N_myon[A:B])#,sigma=np.sqrt(N_myon[A:B]),p0=[2,125281093,0])
+par = unc.correlated_values(par, cov)
+lamda = float(unp.nominal_values(par[0]))
+N = float(unp.nominal_values(par[1]))
+U = float(unp.nominal_values(par[2]))
+lamda_f= float(unp.std_devs(par[0]))
+N_f= float(unp.std_devs(par[1]))
+U_f= float(unp.std_devs(par[2]))
+
+t=np.linspace(5,60)
+
+plt.plot(t,exp(t,lamda,N,U),"m-",label="Fit")
+plt.plot(t_myon[A:B],N_myon[A:B],"bx",label="Messwerte")
+plt.xlabel(r"$t [\mathrm{\mu s}]$")
+plt.yscale("log")
+plt.xlim(0,60)
+plt.legend()
+plt.show()
+
+print(lamda,N,U)
