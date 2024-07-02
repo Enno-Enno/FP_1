9c3b715d5f76c833cd0c1872481a28904e81dc45
Benedikt Lütke Lanfer <benedikt.luetkelanfer@tu-dortmund.de>
2024-07-02 12:07:26 +0200

    Schwierige Auswertung

--- 05_V01/plot1.py
+++ 05_V01/plot1.py
@@ -9,7 +9,7 @@ def f(x, m, b):
     return m*x+b 
 
 def exp(t,lamda,N,U):
-    return N*np.exp(-lamda*t) +U
+    return N*lamda*t*np.exp(-lamda*t) +U
 
 
 N_kal=np.genfromtxt('data2.txt', unpack=True)
@@ -28,6 +28,7 @@ m_f= float(unp.std_devs(par[0]))
 b_f= float(unp.std_devs(par[1]))
 
 print(m,b)
+
 x=np.linspace(0,450)
 
 plt.plot(x,f(x,m,b),"m-",label="Ausgleichsgerade")
@@ -45,25 +46,34 @@ plt.xlabel(r"$t [\mathrm{\mu s}]$")
 plt.legend()
 plt.show()
 
-A=17;B=-1
 
-par, cov=curve_fit(exp,t_myon[A:B],N_myon[A:B])#,sigma=np.sqrt(N_myon[A:B]),p0=[2,125281093,0])
+A=17;B=int(np.floor((10-b)/m)+1)
+print(B)
+
+#par, cov=curve_fit(exp,t_myon[A:B],N_myon[A:B])#,sigma=np.sqrt(N_myon[A:B]),p0=[2,125281093,0])
+#par = unc.correlated_values(par, cov)
+#lamda = float(unp.nominal_values(par[0]))
+#N = float(unp.nominal_values(par[1]))
+#U = float(unp.nominal_values(par[2]))
+#lamda_f= float(unp.std_devs(par[0]))
+#N_f= float(unp.std_devs(par[1]))
+#U_f= float(unp.std_devs(par[2]))
+
+par, cov=curve_fit(f,t_myon[A:B], np.log(N_myon[A:B]))
 par = unc.correlated_values(par, cov)
-lamda = float(unp.nominal_values(par[0]))
-N = float(unp.nominal_values(par[1]))
-U = float(unp.nominal_values(par[2]))
-lamda_f= float(unp.std_devs(par[0]))
-N_f= float(unp.std_devs(par[1]))
-U_f= float(unp.std_devs(par[2]))
+m = float(unp.nominal_values(par[0]))
+b = float(unp.nominal_values(par[1]))
+m_f= float(unp.std_devs(par[0]))
+b_f= float(unp.std_devs(par[1]))
 
-t=np.linspace(5,60)
+t=np.linspace(2,10)
 
-plt.plot(t,exp(t,lamda,N,U),"m-",label="Fit")
-plt.plot(t_myon[A:B],N_myon[A:B],"bx",label="Messwerte")
+plt.plot(t,f(t,m,b),"m-",label="Fit")
+plt.plot(t_myon[A:B],np.log(N_myon[A:B]),"bx",label="Messwerte")
 plt.xlabel(r"$t [\mathrm{\mu s}]$")
-plt.yscale("log")
+#plt.yscale("log")
 plt.xlim(0,60)
 plt.legend()
 plt.show()
 
-print(lamda,N,U)
+print(np.log(2)/m)
